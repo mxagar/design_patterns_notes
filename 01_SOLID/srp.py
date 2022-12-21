c1182530d9d7bf1e358753a79ad9abe1f45eb36b
Mikel <mxagar@gmail.com>
2022-12-21 07:53:44 +0100

    single responsibility principle

--- 01_SOLID/srp.py
+++ 01_SOLID/srp.py
@@ -13,7 +13,14 @@ class Journal:
     def __str__(self):
         return "\n".join(self.entries)
 
-    # break SRP
+    # Break SRP = Single Responsibility Principle
+    # Saving is a responsibility that should be handled
+    # by a class that persists the complete family
+    # of journal classes!
+    # Giving too many responsibilities to single classes
+    # creates the anti-pattern of a God Object:
+    # a gigant class which does everything,
+    # which is very difficult to maintain!
     def save(self, filename):
         file = open(filename, "w")
         file.write(str(self))
@@ -25,8 +32,16 @@ class Journal:
     def load_from_web(self, uri):
         pass
 
-
+# Persisting of objects should be handled by a spacific class
+# which takes care of all faminily of journal classes.
+# That way, the saving processes are all localized
+# in the same spot -> easier to maintain!
 class PersistenceManager:
+    # Static method: it cannot access/modify either the instance or the class
+    # but it can be used from both.
+    # They signal that the function is independent from the class/object,
+    # i.e., some kind of utility procedure; that improves mantainability.
+    @staticmethod
     def save_to_file(journal, filename):
         file = open(filename, "w")
         file.write(str(journal))
@@ -39,7 +54,7 @@ j.add_entry("I ate a bug.")
 print(f"Journal entries:\n{j}\n")
 
 p = PersistenceManager()
-file = r'c:\temp\journal.txt'
+file = r'./journal.txt'
 p.save_to_file(j, file)
 
 # verify!
