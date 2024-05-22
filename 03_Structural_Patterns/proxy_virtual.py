403e7e6e7ccc7f14304ed08c6a9f55dc208b0371
Mikel Sagardia <sagardia.mikel@gmail.com>
2024-05-22 18:03:45 +0200

    feat: proxy examples

---
+++ 03_Structural_Patterns/proxy_virtual.py
@@ -0,0 +1,33 @@
+"""From the Udemy course "Design Patterns in Python"
+https://www.udemy.com/course/design-patterns-python
+by Dmitri Nesteruk.
+
+Structural Patterns: Proxy
+"""
+class Bitmap:
+    def __init__(self, filename):
+        self.filename = filename
+        print(f'Loading image from {filename}')
+
+    def draw(self):
+        print(f'Drawing image {self.filename}')
+
+
+class LazyBitmap:
+    def __init__(self, filename):
+        self.filename = filename
+        self.bitmap = None
+
+    def draw(self):
+        if not self.bitmap:
+            self.bitmap = Bitmap(self.filename)
+        self.bitmap.draw()
+
+def draw_image(image):
+    print('About to draw image')
+    image.draw()
+    print('Done drawing image')
+
+if __name__ == '__main__':
+    bmp = LazyBitmap('facepalm.jpg')  # Bitmap
+    draw_image(bmp)
